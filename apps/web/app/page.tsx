c6fceccc3eebc47d38bce404ba235dc973ce9b10
John Nobody <devonly1984@hotmail.com>
2025-08-08 21:43:30 -0500

    issues with convex

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,14 @@
-import {add} from '@workspace/math/add'
+"use client"
+import {useQuery} from 'convex/react';
+import { api } from '@workspace/backend/convex/_generated/api';
+
+
+
 const WebHomePage = () => {
+  const users = useQuery(api.queries.users.getMany);
+  console.log(users);
   return (
-    <div>{add(15,25)}</div>
+    <div></div>
   )
 }
 export default WebHomePage
