ec2b27147dd7310aec06f8a5b09f7ce9c44220e3
John Nobody <devonly1984@hotmail.com>
2025-09-06 04:23:01 -0500

    Sentry Integration

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 
 import { Button } from "@workspace/ui/components/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { api } from "../../../../packages/backend/convex/_generated/api";
 const WebPage = () => {
-  const users = useQuery(api.public.queries.users.getMany);
   const addUser = useMutation(api.public.mutations.users.add);
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
-        <Button onClick={() => addUser()}>Add User</Button>
         <p>apps/web</p>
         <UserButton />
+        
         <OrganizationSwitcher hidePersonal />
-        <div className="max-w-sm w-full mx-auto">
-          {JSON.stringify(users, null, 2)}
-        </div>
+        <Button onClick={() => addUser()}>Add User</Button>{" "}
       </div>
     </>
   );
