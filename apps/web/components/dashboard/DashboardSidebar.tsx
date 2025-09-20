bb2420a585b40ab6bbcd5d5a6829c8b18b1be11c
John Nobody <devonly1984@hotmail.com>
2025-09-19 21:36:24 -0500

    3:56:21 Middle of Seven2

--- apps/web/components/dashboard/DashboardSidebar.tsx
+++ apps/web/components/dashboard/DashboardSidebar.tsx
@@ -27,7 +27,9 @@ const DashboardSidebar = () => {
               <OrganizationSwitcher
                 hidePersonal
                 skipInvitationScreen
-                appearance={{}}
+                appearance={{
+                    
+                }}
               />
             </SidebarMenuButton>
           </SidebarMenuItem>
